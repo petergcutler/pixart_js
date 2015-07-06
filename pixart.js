4c764d4d8dc79128625da68b5404a24ab20b10d9
Jesse Shawl <jesse@jshawl.com>
2015-07-06 18:51:46 -0400

    #4 - change little square color on click

--- pixart.js
+++ pixart.js
@@ -6,3 +6,9 @@ $("#form")[0].addEventListener("submit", function(event){
 for( var i = 0; i < 20; i++ ){
   $("body").append($("<div class='square'/>"))
 }
+
+document.body.addEventListener("click", function(event){
+  if(event.target.classList.contains("square")){
+    $(event.target).css("background","green") 
+  }
+})
