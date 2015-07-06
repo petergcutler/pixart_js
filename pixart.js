2e1c7c55a06c8f5915e85e2f166a493bf03ec952
Jesse Shawl <jesse@jshawl.com>
2015-07-06 18:48:57 -0400

    #3 - Create 20 divs

--- pixart.js
+++ pixart.js
@@ -2,3 +2,7 @@ $("#form")[0].addEventListener("submit", function(event){
   event.preventDefault()
   $(".brush").css("background",$("#color-field").val())
 })
+
+for( var i = 0; i < 20; i++ ){
+  $("body").append($("<div class='square'/>"))
+}
